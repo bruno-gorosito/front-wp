4d641335e5b73c04c2d66daaa996524eeac1c35e
Bruno Gorosito <gorositobrunoalejandro@gmail.com>
2023-07-12 16:32:46 -0300

    con tiempo real

--- src/config/axios.tsx
+++ src/config/axios.tsx
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const axiosClient = axios.create({
     // baseURL: "https://back-wp.onrender.com/api"
-    baseURL: "http://localhost:4960/api"
+    baseURL: process.env.NEXT_PUBLIC_BACK_AXIOS
 })
